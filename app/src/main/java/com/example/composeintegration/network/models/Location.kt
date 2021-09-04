fdb5d31e285f54d4016f09c5dce7ea422ffe5e27
Krzysztof Dziedzic <34074568+krzychudz@users.noreply.github.com>
2021-09-04 18:34:59 +0200

    Add new fields to User model

---
+++ app/src/main/java/com/example/composeintegration/network/models/Location.kt
@@ -0,0 +1,8 @@
+package com.example.composeintegration.network.models
+
+data class Location(
+    val street: String?,
+    val city: String?,
+    val state: String?,
+    val postcode: String?
+)
--- app/src/main/java/com/example/composeintegration/network/models/User.kt
+++ app/src/main/java/com/example/composeintegration/network/models/User.kt
@@ -3,5 +3,7 @@ package com.example.composeintegration.network.models
 data class User(
     val name: UserName?,
     val picture: UserPicture?,
-    val email: String?
+    val email: String?,
+    val phone: String?,
+    val location: Location?
 )
