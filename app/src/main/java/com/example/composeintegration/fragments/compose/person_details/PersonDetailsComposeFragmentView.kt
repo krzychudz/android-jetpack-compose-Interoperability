0024b578b78f98257df0bfa83c5730a12a7e25a7
Krzysztof Dziedzic <34074568+krzychudz@users.noreply.github.com>
2021-09-05 13:10:09 +0200

    Add aditional info section expand animation

--- app/src/main/java/com/example/composeintegration/fragments/compose/person_details/PersonDetailsComposeFragmentView.kt
+++ app/src/main/java/com/example/composeintegration/fragments/compose/person_details/PersonDetailsComposeFragmentView.kt
@@ -1,14 +1,24 @@
 package com.example.composeintegration.fragments.compose.person_details
 
+import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.animation.ExperimentalAnimationApi
+import androidx.compose.animation.core.LinearEasing
+import androidx.compose.animation.core.animateFloatAsState
+import androidx.compose.animation.core.tween
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material.Card
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ChevronLeft
+import androidx.compose.material.icons.filled.Upload
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.rotate
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -17,21 +27,28 @@ import com.example.composeintegration.R
 import com.example.composeintegration.composables.PlainHeader
 import com.example.composeintegration.network.models.User
 
+@ExperimentalAnimationApi
 @Composable
 fun PersonDetailsComposeScreen(userData: User?) {
     Column(modifier = Modifier.padding(horizontal = 16.dp, vertical = 8.dp)) {
         PlainHeader(stringResource(id = R.string.overview_header))
         PersonOverview(userData = userData)
-        PlainHeader(stringResource(id = R.string.extra_info_header), modifier = Modifier.padding(top = 6.dp))
+        PlainHeader(
+            stringResource(id = R.string.extra_info_header),
+            modifier = Modifier.padding(top = 6.dp)
+        )
+        AdditionalDataSection(userData = userData)
     }
 }
 
 @Composable
 fun PersonOverview(userData: User?) {
     Card(elevation = 4.dp, modifier = Modifier.fillMaxWidth()) {
-        Row(modifier = Modifier
-            .fillMaxWidth()
-            .padding(8.dp)) {
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(8.dp)
+        ) {
             Surface(
                 shape = RoundedCornerShape(50)
             ) {
@@ -46,13 +63,57 @@ fun PersonOverview(userData: User?) {
                     modifier = Modifier.size(128.dp),
                 )
             }
-            Column(modifier = Modifier.padding(horizontal = 4.dp)) {
+            Column(modifier = Modifier.padding(horizontal = 4.dp, vertical = 2.dp)) {
                 val userNameData = userData?.name
                 Text(
-                    text ="${userNameData?.first} ${userNameData?.last}",
+                    text = "${userNameData?.first} ${userNameData?.last}",
                     style = MaterialTheme.typography.h5
                 )
                 Text(userData?.email ?: "", fontSize = 12.sp)
+            }
+        }
+    }
+}
+
+@ExperimentalAnimationApi
+@Composable
+fun AdditionalDataSection(userData: User?) {
+    var isAdditionalInfoExpanded by remember { mutableStateOf(false) }
+
+    val userNameData = userData?.name
+    val chevronAngle: Float by animateFloatAsState(
+        targetValue = if (!isAdditionalInfoExpanded) 270F else 90F,
+        animationSpec = tween(
+            durationMillis = 100,
+            easing = LinearEasing
+        )
+    )
+
+    Card(
+        elevation = 4.dp,
+        modifier = Modifier.fillMaxWidth(),
+        shape = RoundedCornerShape(topStart = 6.dp, topEnd = 6.dp)
+    ) {
+        Column {
+            Row(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .width(16.dp)
+                    .padding(8.dp),
+                horizontalArrangement = Arrangement.SpaceBetween,
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+                Text("${userNameData?.first} ${userNameData?.last}")
+                IconButton(
+                    onClick = { isAdditionalInfoExpanded = !isAdditionalInfoExpanded },
+                    modifier = Modifier.rotate(chevronAngle)
+                ) {
+                    Icon(Icons.Filled.ChevronLeft, "Chevron")
+                }
+            }
+            Divider(color = Color.Black, thickness = 1.dp)
+            AnimatedVisibility(visible = isAdditionalInfoExpanded) {
+                Text("Tewst12121")
             }
         }
     }
